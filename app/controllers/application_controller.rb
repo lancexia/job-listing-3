773f2b5ca449508670dad23eb7af5fd68670ed62
lance <xiakailun919@163.com>
2017-03-18 19:06:49 +0800

    implement part 2

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,12 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+
+  def require_is_admin
+    if !current_user.admin?
+      flash[:alert] = "YOU ARE NOT ADMIN"
+      redirect_to root_path
+    end
+  end
+
+
 end
--- app/controllers/jobs_controller.rb
+++ app/controllers/jobs_controller.rb
@@ -1,6 +1,8 @@
 class JobsController < ApplicationController
   before_action :authenticate_user!, only: [:new, :create, :update, :edit, :destroy ]
 
+
+
   def index
     @jobs = Job.all
   end
